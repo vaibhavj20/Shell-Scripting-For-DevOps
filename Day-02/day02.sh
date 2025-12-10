6cd21bcafd0b678eddc2ab27e5a8becaeec7434e
Vaibhav <vaibhavjam86@gmail.com>
2025-12-10 18:38:23 +0530

    variables and printing name of user

---
+++ Day-02/day02.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+echo "Starting comment block"
+
+<<comment
+hey there
+hello
+how are you?
+comment
+
+
+echo "Comment block finished"
+
+
+name="Satish"
+
+echo "My name is $name"
+
+
+# Take name from user 
+
+echo "Enter your name :"
+
+read username
+
+echo "Hello, $username, Welcome to shell scripting! We will meet on $(date)"
+
+
