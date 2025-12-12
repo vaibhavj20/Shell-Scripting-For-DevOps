1c5107bfc9ac73294584bafc807575a056314936
Vaibhav <vaibhavjam86@gmail.com>
2025-12-12 10:07:38 +0530

    modified

--- Day-02/func.sh
+++ Day-02/func.sh
@@ -4,6 +4,31 @@
 Check the age of a person for voting eligibility.
 task
 
+read -p "Enter you name: " name
+read -p "Enter you age: " age
+
+function check_voting_eligibility() {
+  
+  if [[ $age -ge 18 ]]; then
+        echo "Hello $name, you are eligible to vote."
+    else
+        echo "Hello $name, you are not eligible to vote."
+    fi
+
+
+}
+
+check_voting_eligibility
+
+#-------------------Functions with Arguments-------------------------------
+
+
+#!/bin/bash
+
+<<task
+Check the age of a person for voting eligibility.
+task
+
 
 function check_voting_eligibility() {
   
@@ -17,5 +42,4 @@ function check_voting_eligibility() {
 }
 
 check_voting_eligibility "Satish" 14
-
 
