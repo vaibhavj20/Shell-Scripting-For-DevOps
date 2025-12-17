af0779c513eb37266f7a6750373665b216f93edb
Vaibhav <vaibhavjam86@gmail.com>
2025-12-17 14:44:17 +0530

    loop to iterate over array

---
+++ Day-06/array-loop.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+fruits=("apple" "banana" "cherry" "pineapple")
+
+for fruit in "${fruits[@]}"; do
+    echo "I like $fruit"
+done
+
+echo "----------------------------"
+fruits+=("grapes")
+
+echo "----------------------------"
+
+
+for fruit in "${fruits[@]}"; do
+    echo "I like $fruit"
+done
+
+echo "----------------------------"
+
+#unset element at index 1 (banana)
+
+# removing all
+
+echo "Removing all elements"
+unset fruits[@]
+
+for fruit in "${fruits[@]}"; do
+    echo "I like $fruit"
+done
+
+
+#removed all elements
+
+echo "Removed all elements"
