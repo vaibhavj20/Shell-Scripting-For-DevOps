dadc34ee5171d40e22167677a387af94a4318cb6
Vaibhav <vaibhavjam86@gmail.com>
2025-12-20 20:15:40 +0530

    Added another example to understand array

--- Day-06/array.sh
+++ Day-06/array.sh
@@ -2,14 +2,33 @@
 
 my_array=("apple" "ball" "cat" "dog")
 
-#priting 3rd element
+# printing 3rd element
 echo ${my_array[2]}
 
-
-#printing entire array
-
+# printing entire array
 echo ${my_array[@]}
 
-#printing length of array
-
+# printing length of array
 echo ${#my_array[@]}
+
+echo "----------------------------"
+
+# 👉 Another example (numbers array)
+
+numbers=(10 20 30 40 50)
+
+# print first element
+echo "First number: ${numbers[0]}"
+
+# print all numbers
+echo "All numbers: ${numbers[@]}"
+
+# print array length
+echo "Total numbers: ${#numbers[@]}"
+
+# loop through array
+echo "Looping through numbers:"
+for num in "${numbers[@]}"
+do
+  echo $num
+done
